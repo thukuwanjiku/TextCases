2e6faa07c89fbae54a05372aa750ffb17cf6ff57
thukuwanjiku <mikedegr809@gmail.com>
2018-01-31 14:50:30 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    form;
+    ngOnInit(){
+    	this.form = new FormGroup({
+    		userText: new FormControl(""),
+    		upperCaseText: new FormControl(""),
+    		lowerCaseText: new FormControl(""),
+    		titleCaseText: new FormControl("")
+    	});
+    }
+
+    textChanged(value){
+    	if(value != ""){
+    		this.form.patchValue({upperCaseText: value.toUpperCase()});
+    		this.form.patchValue({lowerCaseText: value.toLowerCase()});
+    		this.form.patchValue({titleCaseText: this.titleCase(value)});
+    	}else{
+    		this.form.patchValue({upperCaseText: ""});
+    		this.form.patchValue({lowerCaseText: ""});
+    		this.form.patchValue({titleCaseText: ""});
+    	}
+    }
+
+    titleCase(string){
+    	return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
 }
